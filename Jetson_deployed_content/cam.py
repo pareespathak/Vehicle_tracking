da1518ce828646c4c51f36f1202964506481304b
Pareese Pathak <pareespathak@gmail.com>
2022-03-13 14:50:47 +0530

    Add files via upload

---
+++ Jetson_deployed_content/cam.py
@@ -0,0 +1,50 @@
+import cv2
+
+'''
+camera paraeters
+'''
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+try:
+
+    capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    ret, frame = capture.read()
+except:
+    print("error in opening cam")
+try:
+    while(ret == True):     ### change later 
+        ret, frame = capture.read()
+        if not ret:
+            break
+
+        cv2.imshow('Press (q) to stop detections', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.imwrite('/home/parees/Downloads/Vehicle_tracking-main/Jetson/device/detection.jpg', frame)
+except:
+    print("main loop error")
